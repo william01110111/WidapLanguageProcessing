34615d98f6728498c04fd20f6e2b65846d5c70c8
William W Wold <wilcoe@rocketmail.com>
2016-03-09 21:07:08 -0800

    added visual output for node structure

--- WidapNLP/src/main/java/net/widap/nlp/TextParser.java
+++ WidapNLP/src/main/java/net/widap/nlp/TextParser.java
@@ -1,6 +1,7 @@
 package net.widap.nlp;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class TextParser
 {
@@ -104,15 +105,35 @@ public class TextParser
 		if (out!=null)
 			return out;
 		
-		Idea idea=parseIdeas(str2Ideas(input));
+		//Node nodeStrt=str2Ideas(input);
+		Node nodeStrt=new Node();
 		
-		if (idea==null)
+		if (WidapMind.lotsOfChecks)
+			nodeStrt.checkStructure();
+		
+		//parseIdeas(nodeStrt);
+		
+		Node n0=nodeStrt, n1=new Node(), n2=new Node(), n3=new Node(), n4=new Node();
+		
+		new Idea("0-1", n0, n1              );
+		new Idea("1-2",     n1, n2          );
+		new Idea("2-3",         n2, n3      );
+		new Idea("3-4",             n3, n4  );
+		new Idea("0-2", n0,     n2          );
+		new Idea("0-1", n0, n1              );
+		new Idea("2-4",         n2,     n4  );
+		
+		if (WidapMind.lotsOfChecks)
+			nodeStrt.checkStructure();
+		
+		if (!nodeStrt.hasIdeas())
 		{
-			out="[parseIdeas() failed to parse '" + input + "']";
+			out="[parseIdeas() failed to parse '"+input+"']";
 		}
 		else
 		{
-			out=idea.toString();
+			//out=nodeStrt.toStringList();
+			out=nodeStrt.toStringVisual();
 		}
 		
 		return out;
@@ -134,6 +155,26 @@ public class TextParser
 			output="goodbye";
 			mind.setQuit(true);
 		}
+		else if (input.equals("what does a tie fighter look like"))
+		{
+			output+="\n┏┓        ┏━━━━━━━┓        ┏┓\n"+
+					"┃┃     ┏━━┛       ┗━━┓     ┃┃\n"+
+					"┃┃    ┏┛   ┏━━━━━┓   ┗┓    ┃┃\n"+
+					"┃┃    ┃  ┏━┛     ┗━┓  ┃    ┃┃\n"+
+					"┃┣━━━━┫  ┃         ┃  ┣━━━━┫┃\n"+
+					"┃┣━━━━┫  ┗━┓     ┏━┛  ┣━━━━┫┃\n"+
+					"┃┃    ┃    ┗━━━━━┛    ┃    ┃┃\n"+
+					"┃┃    ┗┓    ┏┓ ┏┓    ┏┛    ┃┃\n"+
+					"┃┃     ┗━━┓ ┗┻━┻┛ ┏━━┛     ┃┃\n"+
+					"┗┛        ┗━━━━━━━┛        ┗┛";
+		}
+		else if (
+				input.equals("what is the answer") ||
+						input.equals("what is the answer to life, the universe and everything") ||
+						input.equals("what is the meaning of life"))
+		{
+			output+="42";
+		}
 		else if (chkPtrn(input, "[w]", words))
 		{
 			Word word;
@@ -144,15 +185,17 @@ public class TextParser
 				output+="I don't know that word.";
 			else
 			{
-				output+="I know that word!\n";
+				output+="\nI know the word '"+words.get(0)+"':";
 				
 				for (int i=0; i<word.variants.size(); i++)
 				{
 					output+="\n"+word.variants.get(i).txt+" ("+word.variants.get(i).pos.name()+")";
 				}
+				
+				output+="\n";
 			}
 		}
-		else if (chkPtrn(input, "[w] are [w]", words))
+		/*else if (chkPtrn(input, "[w] are [w]", words))
 		{
 			Thing thing, type;
 			String name, typeName;
@@ -275,7 +318,7 @@ public class TextParser
 					output+='.';
 				}
 			}
-		}
+		}*/
 		else if (chkPtrn(input, "what is [w]", words) || chkPtrn(input, "what is a [w]", words))
 		{
 			ArrayList<Thing> things=mind.getThings(words.get(0));
@@ -375,11 +418,9 @@ public class TextParser
 		return output;
 	}
 	
-	private Idea parseIdeas(Node start)
+	private void parseIdeas(Node start)
 	{
-		
-		
-		return null;
+		start.splitAll();
 	}
 	
 	private Node str2Ideas(String inStr)
@@ -482,7 +523,7 @@ public class TextParser
 		return out;
 	}
 	
-	static class Node //nodes connect ideas in a complex data structure
+	private class Node //nodes connect ideas in a complex data structure
 	{
 		//in the data structure, by starting at the start node and at each node going to one of the 'next' Ideas and it's 'next' node,
 		//you will eventually get to the end (represented by a node with a next ArrayList with length 0)
@@ -508,7 +549,7 @@ public class TextParser
 		{
 			Node end=this;
 			int i=0;
-			while (end.next.size()>0) 
+			while (end.next.size()>0)
 			{
 				//stop endless loops if the data structure is messed up
 				if (WidapMind.lotsOfChecks)
@@ -563,6 +604,335 @@ public class TextParser
 			return start;
 		}
 		
+		//if this is part of a structure that contains at least one idea
+		public boolean hasIdeas()
+		{
+			return prev.size()>0 || next.size()>0;
+		}
+		
+		public void splitAll()
+		{
+			int i;
+			
+			for (i=0; i<next.size(); i++)
+			{
+				Idea idea=next.get(i);
+				idea.split();
+				//idea.next.splitAll();
+			}
+		}
+		
+		//has issues
+		//turn this entire data structure into a multi-line human readable string
+		public String toStringList()
+		{
+			//make sure it is the start node of the structure
+			if (prev.size()>0)
+				return getStart().toStringList();
+			
+			return "all possible node system paths:\n"+listStructInternal();
+		}
+		
+		private String listStructInternal()
+		{
+			String out="";
+			
+			if (next.size()==0)
+				return "[end]";
+			
+			if (prev.size()==0)
+				out+="[start] -> ";
+			
+			for (int i=0; i<next.size(); i++)
+			{
+				if (i>0)
+					out+="\n";
+				Idea idea=next.get(i);
+				out+=idea.toString()+" -> "+idea.next.listStructInternal();
+			}
+			
+			return out;
+		}
+		
+		//display the entire data structure visually using text
+		public String toStringVisual()
+		{
+			//below are the thick unicode box drawing characters that can be copied from
+			/*retrieved from http://unicode-table.com/en/blocks/box-drawing/
+			━ ┃ ┏ ┓ ┗ ┛ ┣ ┫ ┳ ┻ ╋
+			
+			
+			┏┓        ┏━━━━━━━┓        ┏┓
+			┃┃     ┏━━┛       ┗━━┓     ┃┃
+			┃┃    ┏┛   ┏━━━━━┓   ┗┓    ┃┃
+			┃┃    ┃  ┏━┛     ┗━┓  ┃    ┃┃
+			┃┣━━━━┫  ┃         ┃  ┣━━━━┫┃
+			┃┣━━━━┫  ┗━┓     ┏━┛  ┣━━━━┫┃
+			┃┃    ┃    ┗━━━━━┛    ┃    ┃┃
+			┃┃    ┗┓             ┏┛    ┃┃
+			┃┃     ┗━━┓       ┏━━┛     ┃┃
+			┗┛        ┗━━━━━━━┛        ┗┛
+			 */
+			
+			//yes, that was absolutely necessary in order to test the continuity of the symbols in a practical application
+			
+			ArrayList<VisualSect> sects=new ArrayList<>();
+			getStart().populateSectList(sects);
+			
+			ArrayList<String> list0=new ArrayList<>();
+			list0.add("[START]━");
+			sects.add(new VisualSect(new Node(), getStart(), list0));
+			
+			ArrayList<String> list1=new ArrayList<>();
+			list1.add("━[END]");
+			sects.add(new VisualSect(getEnd(), new Node(), list1));
+			
+			WidapMind.message("sect list size: "+sects.size());
+			
+			boolean stop;
+			
+			do
+			{
+				stop=true;
+				
+				for (int i=0; i<sects.size(); i++)
+				{
+					VisualSect sect=sects.get(i);
+					
+					boolean invalid=false;
+					VisualSect other=null;
+					
+					for (int j=0; j<sects.size() && !invalid; j++)
+					{
+						if (j!=i)
+						{
+							if (sects.get(j).end==sect.end)
+								invalid=true;
+							
+							if (sect.end==sects.get(j).strt)
+							{
+								if (other==null)
+								{
+									other=sects.get(j);
+								}
+								else
+								{
+									invalid=true;
+								}
+							}
+						}
+					}
+					
+					if (other!=null && !invalid)
+					{ //merge them
+						
+						stop=false; //if this iteration did anything, stop must be set to false
+						
+						ArrayList<String> lines=new ArrayList<>();
+						
+						int lineNum=Math.max(sect.lines.size(), other.lines.size());
+						
+						for (int j=0; j<lineNum; j++)
+						{
+							String l0, l1;
+							
+							if (j<sect.lines.size())
+								l0=sect.lines.get(j);
+							else
+							{
+								l0="";
+								
+								for (int k=0; k<sect.lines.get(0).length(); k++)
+									l0+=" ";
+							}
+							
+							if (j<other.lines.size())
+								l1=other.lines.get(j);
+							else
+							{
+								l1="";
+								
+								for (int k=0; k<other.lines.get(0).length(); k++)
+									l1+=" ";
+							}
+							
+							lines.add(l0+l1);
+						}
+						
+						sects.set(i, new VisualSect(sect.strt, other.end, lines));
+						sects.remove(other);
+					}
+				}
+				
+				for (int i=0; i<sects.size(); i++)
+				{
+					VisualSect sect=sects.get(i);
+					
+					ArrayList<VisualSect> others=new ArrayList<>();
+					
+					for (int j=0; j<sects.size(); j++)
+					{
+						//it is ok that j will equal i once
+						
+						VisualSect other=sects.get(j);
+						
+						if (other.strt==sect.strt && other.end==sect.end)
+						{
+							others.add(other);
+						}
+					}
+					
+					if (others.size()>1)
+					{ //merge them
+						
+						stop=false; //if this iteration did anything, stop must be set to false
+						
+						ArrayList<String> lines=new ArrayList<>();
+						
+						int maxLng=0;
+						
+						for (VisualSect other : others)
+							maxLng=Math.max(other.lines.get(0).length(), maxLng);
+						
+						for (int j=0; j<others.size(); j++)
+						{
+							ArrayList<String> oLines=others.get(j).lines;
+							String linePad="", spacePad="";
+							
+							//━ ┃ ┏ ┓ ┗ ┛ ┣ ┫ ┳ ┻ ╋
+							
+							for (int k=0; k<maxLng-oLines.get(0).length(); k++)
+							{
+								spacePad+=" ";
+								linePad+="━";
+							}
+							
+							for (int k=0; k<oLines.size(); k++)
+							{
+								String str=oLines.get(k);
+								String strt, end;
+								String pad;
+								
+								if (k==0)
+								{
+									pad=linePad;
+									
+									if (j==0)
+									{
+										if (others.size()>1)
+										{
+											strt="┳";
+											end="┳";
+										}
+										else
+										{
+											strt="━";
+											end="━";
+										}
+									}
+									else if (j<others.size()-1)
+									{
+										strt="┣";
+										end="┫";
+									}
+									else
+									{
+										strt="┗";
+										end="┛";
+									}
+								}
+								else
+								{
+									pad=spacePad;
+									
+									if (j<others.size()-1)
+									{
+										strt="┃";
+										end="┃";
+									}
+									else
+									{
+										strt=" ";
+										end=" ";
+									}
+								}
+								
+								lines.add(strt+str+pad+end);
+							}
+						}
+						
+						for (VisualSect other : others)
+							sects.remove(other);
+						
+						sects.add(i, new VisualSect(sect.strt, sect.end, lines));
+					}
+				}
+			}
+			while (!stop);
+			
+			if (sects.size()==1)
+			{
+				String comp="node structure diagram:\n";
+				
+				for (String line : sects.get(0).lines)
+				{
+					comp+=line+"\n";
+				}
+				
+				return comp;
+			}
+			else 
+			{
+				String comp="data structure visualizer failed, here are the incomplete sects::\n";
+				
+				for (VisualSect sect : sects)
+				{
+					for (String line : sect.lines) 
+					{
+						comp+=line+"\n";
+					}
+				}
+				
+				return comp;
+			}
+		}
+		
+		//section in the visual display of the node structure
+		class VisualSect
+		{
+			public final Node strt, end;
+			public final ArrayList<String> lines;
+			
+			VisualSect(Node s, Node e, ArrayList<String> l)
+			{
+				strt=s;
+				end=e;
+				lines=l;
+			}
+		}
+		
+		private void populateSectList(ArrayList<VisualSect> sects)
+		{
+			for (Idea idea : next)
+			{
+				ArrayList<String> lines=new ArrayList<>();
+				lines.add("━["+idea.toString()+"]━");
+				sects.add(new VisualSect(this, idea.next, lines));
+				
+				boolean dupli=false;
+				for (VisualSect sect : sects)
+				{
+					if (sect.strt==idea.next)
+						dupli=true;
+				}
+				
+				if (!dupli)
+				{
+					idea.next.populateSectList(sects);
+				}
+			}
+		}
+		
 		//checks to make sure everything is right in the data structure this starts (will have error if this is not the starting node)
 		public void checkStructure()
 		{
@@ -584,9 +954,8 @@ public class TextParser
 				if (node==this)
 				{
 					WidapMind.errorMsg("loop detected in node data structure in text parser. THIS IS VERY BAD!!!");
+					return end;
 				}
-				
-				return end;
 			}
 			
 			nodes.add(this);
@@ -618,27 +987,29 @@ public class TextParser
 		}
 	}
 	
-	static class Idea //an idea that can be anything from an unrecognised word to a thing with attributes and actions
+	private class Idea //an idea that can be anything from an unrecognised word to a thing with attributes and actions
 	{
 		Node next, prev;
 		
 		String str;
 		Word.Variant variant;
 		boolean plural;
-		ArrayList<Thing> things;
+		//ArrayList<Thing> things;
+		Thing thing;
 		
 		//if either is null, the other is only used to find the correct data structure, which node it is doesn't matter
 		//if n0 is null, inserts this idea at the very beginning and creates new 2nd node to come after it that contains all the pointers the old beginning node had
-			//old beginning node pointers stay valid and still point to the first node
+		//old beginning node pointers stay valid and still point to the first node
 		//if n1 is null, inserts this idea at the very end and creates a new 2nd-to-last node which contains the same previous pointers as the old end node
-			//old end node pointers stay valid and still point to the last node,
-			//unless there was originally only one node in the whole structure, in which case that becomes the first
+		//old end node pointers stay valid and still point to the last node,
+		//unless there was originally only one node in the whole structure, in which case that becomes the first
 		//if neither is null and they are both different, this is inserted between them without creating any nodes
 		//if they are the same and not null, that node is split (the original becomes the first of the two) and this is inserted
 		//if both are null, and error is thrown
 		public Idea(Node n0, Node n1)
 		{
-			things=new ArrayList<>();
+			//things=new ArrayList<>();
+			thing=null;
 			variant=null;
 			plural=false;
 			str=null;
@@ -717,10 +1088,41 @@ public class TextParser
 			addBetween(n0, n1);
 		}
 		
+		//splits the entire structure into 
+		public void split()
+		{
+			WidapMind.message(toString()+" split.");
+			
+			if (thing==null && variant==null)
+			{
+				ArrayList<Word.Variant> variants=dict.getVariants(str);
+				
+				for (Word.Variant v : variants)
+				{
+					if (v!=variant)
+					{
+						Idea idea=new Idea(str, prev, next);
+						idea.variant=v;
+					}
+				}
+				
+				ArrayList<Thing> things=mind.getThings(str);
+				
+				for (Thing t : things)
+				{
+					if (t!=thing)
+					{
+						Idea idea=new Idea(str, prev, next);
+						idea.thing=t;
+					}
+				}
+			}
+		}
+		
 		public String toString()
 		{
 			String out="[no data in Idea]";
-			if (things.size()>0)
+			/*if (things.size()>0)
 			{
 				ArrayList<String> list=new ArrayList<>();
 				for (Thing thing : things)
@@ -728,6 +1130,10 @@ public class TextParser
 					list.add(thing.toString());
 				}
 				out=combineList(list);
+			}*/
+			if (thing!=null)
+			{
+				out=thing.toString();
 			}
 			else if (variant!=null)
 			{
@@ -735,7 +1141,7 @@ public class TextParser
 			}
 			else if (str!=null)
 			{
-				out=str;
+				out="'"+str+"'";
 			}
 			
 			return out;
